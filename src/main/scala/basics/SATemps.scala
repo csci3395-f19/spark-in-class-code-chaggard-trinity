c0881ebd566b56b36fb36cef1229f7ac904f69d9
Caroline Haggard <chaggard@trinity.edu>
2019-09-10 11:24:22 -0500

    Late commit. Trying to catch up

--- src/main/scala/basics/SATemps.scala
+++ src/main/scala/basics/SATemps.scala
@@ -1,4 +1,8 @@
 package basics
+import swiftvis2.plotting._
+import swiftvis2.plotting.styles.ScatterStyle
+import swiftvis2.plotting.renderer.SwingRenderer
+import swiftvis2.plotting.ColorGradient
 
 case class TempRow(day: Int, doy: Int, month: Int, year: Int, precip: Double,
     tave: Double, tmax: Double, tmin: Double)
@@ -32,8 +36,24 @@ object SATemps{
     println((wetdaycount.toDouble / data.length.toDouble)*100)
 
     val wetdays = data.filter(x => x.precip > 0.0)
-    val hotWet = wetdays.map(_.tmax).sum / wetdays.length
-    println(hotWet)
+    val hotWet = wetdays.foldLeft(0.0)(_ + _.tmax) / wetdays.length
+    val (rainySum, rainyCount) = data.foldLeft((0.0,0)) { case ((sum, cnt), day) =>
+      if(day.precip >= 1.0) (sum + day.precip, cnt + 1) else (sum, cnt)
     }
+    println(rainySum/rainyCount)
+    
+    val months = data.groupBy(_.month)
+    val avgHiTempPerMonths =months.mapValues((rows) => rows.foldLeft(0.0)(_+_.tmax)/rows.length)
+    avgHiTempPerMonths.toSeq.sortBy(_._1).foreach(println)
+    
+    val medianPrecipByMonth = months.mapValues(m => m.sortBy(_.precip).apply(m.length/2))
+    medianPrecipByMonth.toSeq.sortBy(_._1).foreach(println)
+
+    val cg = ColorGradient(1946.0 -> RedARGB, 1975.0 -> BlueARGB, 2014.0 -> GreenARGB)
+    val tempByDayPlot = Plot.simple(
+      ScatterStyle(data.map(_.doy), data.map(_.tave)), colors = cg(data.map(_year))),
+      "SA Temps", "Day of Year", "Temp")
+    SwingRenderer(tempByDayPlot, 800,800, true)
+  }
 
 }
